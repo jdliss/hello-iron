a93772eaedf9201cdebecec6f49ae5c9414c464c
Jonathan <jonacom@lissismore.com>
2016-09-10 19:38:47 -0400

    stuck with router post error

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,39 @@
 extern crate iron;
 extern crate rustc_serialize;
+extern crate router;
 
 use iron::prelude::*;
 use iron::status;
 use rustc_serialize::json;
+use router::Router;
+use std::io::Read;
 
-#[derive(RustcEncodable)]
+#[derive(RustcEncodable, RustcDecodable)]
 struct Greeting {
     msg: String
 }
 
 fn main() {
+    let mut router = Router::new();
+
+    router.get("/", hello_world);
+    router.post("/set", set_greeting);
+
     fn hello_world(_: &mut Request) -> IronResult<Response> {
         let greeting = Greeting { msg: "Hello, World!".to_string() };
         let payload = json::encode(&greeting).unwrap();
         Ok(Response::with((status::Ok, payload)))
     }
 
+    fn set_greeting(request: &mut Request) -> IronResult<Response> {
+        let mut payload = String::new();
+        request.body.read_to_string(&mut payload).unwrap();
+        let request: Greeting = json::decode(&payload).unwrap();
+        let greeting = Greeting { msg: request.msg };
+        let payload = json::encode(&greeting).unwrap();
+        Ok(Response::with((status::Ok, payload)))
+    }
+
     println!("Server running on localhost:3000");
-    Iron::new(hello_world).http("localhost:3000").unwrap();
+    Iron::new(router).http("localhost:3000").unwrap();
 }
